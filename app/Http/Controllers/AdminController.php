2e397feccd5da9cefe325e4ea6e9a40e35a6a1d3
5D Developer <5ddeveloper247@gmail.com>
2025-04-11 10:32:32 +0500

    11-04-2025

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -1760,7 +1760,7 @@ class AdminController extends Controller
             'description' => 'required',
             'address' => 'required',
             'rating' => 'required|numeric|between:1,5',
-            'profile' => 'nullable|image|mimes:jpeg,png,jpg|max:10240'
+            'profile' => 'required|image|mimes:jpeg,png,jpg|max:10240'
         ]);
 
         $testimonial = new Testimonial();
